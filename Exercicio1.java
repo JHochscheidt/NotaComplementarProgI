8de94f48b3001b170f9f49db6e1fc8a2c4169e55
Sival Junior <jr_sival@hotmail.com>
2015-06-26 08:18:06 -0300

    arquivos com a classe Numero

---
+++ Exercicio1.java
@@ -0,0 +1,19 @@
+// Faça um programa que imprima na tela os N primeiros numeros primos existentes.
+// O usuário informa N. 
+
+import java.util.Scanner;
+
+class Exercicio1 {
+	
+		public static void main(String[] args){
+		
+		Scanner s = new Scanner(System.in);
+		
+				System.out.println("Informe um valor para N");
+				
+			
+		
+		}
+
+}
+
